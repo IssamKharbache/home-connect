4dd20815d20a1c1c48a217d62686d3a1175fdac2
lefline <issamkharbache2@gmail.com>
2024-09-24 12:43:47 +0100

    creating the header

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,22 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <section className="h-wrapper">
+      <div className="flexCenter paddings innderWidth h-container">
+        <img src="./logo1.png" alt="Logo" width={100} />
+        <div className="flexCenter h-menu">
+          <a href="">Properties</a>
+          <a href="">Our Value</a>
+          <a href="">Contact us</a>
+          <a href="">Get Started</a>
+          <button className="button">
+            <a href="">Contact</a>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
