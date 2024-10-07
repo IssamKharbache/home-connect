cb4d3310bfe0d5f59430e51da8ea88117660b192
lefline <issamkharbache2@gmail.com>
2024-10-07 15:29:38 +0100

    editing base url to the deployed url

--- client/src/utils/apiCalls/api.js
+++ client/src/utils/apiCalls/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "sonner";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://home-connect-project.vercel.app/api",
 });
 
 // get all properties function
